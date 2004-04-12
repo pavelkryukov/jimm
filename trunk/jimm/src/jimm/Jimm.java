7610b3d625830fed2db88eef43c4927fe1cdc555
linsmayer <linsmayer@9db0892b-294c-0410-b6c9-f1a9b70915b5>
2004-04-12 15:40:32 +0000

    Code cleanup mod_TRAF renamed modules_TRAFFIC

--- trunk/jimm/src/jimm/Jimm.java
+++ trunk/jimm/src/jimm/Jimm.java
@@ -40,182 +40,190 @@ public class Jimm extends MIDlet
 {
 
 
-  // Version
-  public static String VERSION;
+	// Version
+	public static String VERSION;
 
 
-  // Application main object
-  public static Jimm jimm;
+	// Application main object
+	public static Jimm jimm;
 
 
-  // Display object
-  public static Display display;
+	// Display object
+	public static Display display;
 
 
-  /****************************************************************************/
+	/****************************************************************************/
 
 
-  // ICQ object
-  private Icq icq;
+	// ICQ object
+	private Icq icq;
 
 
-  // Options container
-  private Options o;
+	// Options container
+	private Options o;
 
 
-  // Main menu object
-  private MainMenu mm;
+	// Main menu object
+	private MainMenu mm;
 
 
-  // Contact list object
-  private ContactList cl;
+	// Contact list object
+	private ContactList cl;
 
 
-  // Timer object
-  private Timer t;
-
-//#sijapp cond.if mod_TRAF is "true" #
-  // Traffic counter
-  private Traffic traffic;
-//#sijapp cond.end#
+	// Timer object
+	private Timer t;
 
 
-  // Splash canvas object
-  private SplashCanvas sc;
+	// #sijapp cond.if modules_TRAFFIC is "true" #
+	// Traffic counter
+	private Traffic traffic;
+	// #sijapp cond.end#
 
 
-  // Start Jimm
-  public void startApp() throws MIDletStateChangeException
-  {
-
-    // Return if MIDlet has already been initialized
-    if (Jimm.jimm != null) return;
-
-    // Get Jimm version
-    Jimm.VERSION = this.getAppProperty("Jimm-Version");
-    if (Jimm.VERSION == null) Jimm.VERSION = "###VERSION###";
-
-    // Create splash canvas object
-    this.sc = new SplashCanvas();
-    this.sc.setMessage(ResourceBundle.getString("jimm.res.Text", "loading"));
-
-    // Check available heap memory, display warning if less then 250 KB
-    if (Runtime.getRuntime().totalMemory() < 256000)
-    {
-      Alert errorMsg = new Alert(ResourceBundle.getString("jimm.res.Text", "warning"), JimmException.getErrDesc(160, 0), null, AlertType.WARNING);
-      errorMsg.setTimeout(Alert.FOREVER);
-      Display.getDisplay(this).setCurrent(errorMsg, this.sc);
-      try
-      {
-        Thread.sleep(3000);
-      }
-      catch (InterruptedException e)
-      {
-        // Do nothing
-      }
-    }
-    // Display splash canvas
-    else
-    {
-      Display.getDisplay(this).setCurrent(this.sc);
-    }
-
-    // Save MIDlet reference
-    Jimm.jimm = this;
-    // Get display object (and update progress indicator)
-    Jimm.display = Display.getDisplay(this);
-    this.sc.setProgress(10);
-    // Create ICQ object (and update progress indicator)
-    this.icq = new Icq();
-    this.sc.setProgress(20);
-    // Create options container (and update progress indicator)
-    this.o = new Options();
-    this.sc.setProgress(40);
-    // Initialize main menu object (and update progress indicator)
-    this.mm = new MainMenu();
-    this.sc.setProgress(50);
-//	#sijapp cond.if mod_TRAF is "true" #
-    // Create traffic Object (and update progress indicator)
-    this.traffic = new Traffic();
-    this.sc.setProgress(60);
-//	#sijapp cond.end#
-    // Create contact list object (and update progress indicator)
-    this.cl = new ContactList();
-    this.sc.setProgress(70);
-    // Create timer object (and update progress indicator)
-    this.t = new Timer();
-    this.sc.setProgress(80);
-    this.sc.setProgress(90);
-    // Activate main menu
-    this.mm.activate();
-
-  }
+	// Splash canvas object
+	private SplashCanvas sc;
 
 
-  // Pause
-  public void pauseApp()
-  {
-    // Do nothing
-  }
+	// Start Jimm
+	public void startApp() throws MIDletStateChangeException
+	{
+
+		// Return if MIDlet has already been initialized
+		if (Jimm.jimm != null) return;
+
+		// Get Jimm version
+		Jimm.VERSION = this.getAppProperty("Jimm-Version");
+		if (Jimm.VERSION == null) Jimm.VERSION = "###VERSION###";
+
+		// Create splash canvas object
+		this.sc = new SplashCanvas();
+		this.sc.setMessage(ResourceBundle.getString("jimm.res.Text", "loading"));
+
+		// Check available heap memory, display warning if less then 250 KB
+		if (Runtime.getRuntime().totalMemory() < 256000)
+		{
+			Alert errorMsg = new Alert(ResourceBundle.getString("jimm.res.Text", "warning"), JimmException.getErrDesc(160, 0), null, AlertType.WARNING);
+			errorMsg.setTimeout(Alert.FOREVER);
+			Display.getDisplay(this).setCurrent(errorMsg, this.sc);
+			try
+			{
+				Thread.sleep(3000);
+			}
+			catch (InterruptedException e)
+			{
+				// Do nothing
+			}
+		}
+		// Display splash canvas
+		else
+		{
+			Display.getDisplay(this).setCurrent(this.sc);
+		}
+
+		// Save MIDlet reference
+		Jimm.jimm = this;
+
+		// Get display object (and update progress indicator)
+		Jimm.display = Display.getDisplay(this);
+		this.sc.setProgress(10);
+
+		// Create ICQ object (and update progress indicator)
+		this.icq = new Icq();
+		this.sc.setProgress(20);
+
+		// Create options container (and update progress indicator)
+		this.o = new Options();
+		this.sc.setProgress(40);
+
+		// Initialize main menu object (and update progress indicator)
+		this.mm = new MainMenu();
+		this.sc.setProgress(50);
+
+		// #sijapp cond.if modules_TRAFFIC is "true" #
+		// Create traffic Object (and update progress indicator)
+		this.traffic = new Traffic();
+		this.sc.setProgress(60);
+		// #sijapp cond.end#
+
+		// Create contact list object (and update progress indicator)
+		this.cl = new ContactList();
+		this.sc.setProgress(70);
+
+		// Create timer object (and update progress indicator)
+		this.t = new Timer();
+		this.sc.setProgress(90);
+
+		// Activate main menu
+		this.mm.activate();
+
+	}
 
 
-  // Destroy Jimm
-  public void destroyApp(boolean unconditional) throws MIDletStateChangeException
-  {
-    Jimm.display.setCurrent(null);
-    this.notifyDestroyed();
-  }
+	// Pause
+	public void pauseApp()
+	{
+		// Do nothing
+	}
 
 
-  // Returns a reference to ICQ object
-  public Icq getIcqRef()
-  {
-    return (this.icq);
-  }
+	// Destroy Jimm
+	public void destroyApp(boolean unconditional) throws MIDletStateChangeException
+	{
+		Jimm.display.setCurrent(null);
+		this.notifyDestroyed();
+	}
 
 
-  // Returns a reference to options container
-  public Options getOptionsRef()
-  {
-    return (this.o);
-  }
+	// Returns a reference to ICQ object
+	public Icq getIcqRef()
+	{
+		return (this.icq);
+	}
 
 
-  // Returns a reference to the main menu object
-  public MainMenu getMainMenuRef()
-  {
-    return (this.mm);
-  }
+	// Returns a reference to options container
+	public Options getOptionsRef()
+	{
+		return (this.o);
+	}
 
 
-  // Returns a reference to the contact list object
-  public ContactList getContactListRef()
-  {
-    return (this.cl);
-  }
+	// Returns a reference to the main menu object
+	public MainMenu getMainMenuRef()
+	{
+		return (this.mm);
+	}
 
 
-  // Returns a reference to the timer object
-  public Timer getTimerRef()
-  {
-    return (this.t);
-  }
+	// Returns a reference to the contact list object
+	public ContactList getContactListRef()
+	{
+		return (this.cl);
+	}
 
 
-  // Returns a reference to splash canvas object
-  public SplashCanvas getSplashCanvasRef()
-  {
-    return (this.sc);
-  }
+	// Returns a reference to the timer object
+	public Timer getTimerRef()
+	{
+		return (this.t);
+	}
 
 
-  // Return a reference to traffic object
-//#sijapp cond.if mod_TRAF is "true" #
-  public Traffic getTrafficRef()
-  {
-    return (this.traffic);
-  }
-//#sijapp cond.end#
+	// Returns a reference to splash canvas object
+	public SplashCanvas getSplashCanvasRef()
+	{
+		return (this.sc);
+	}
+
+
+	// #sijapp cond.if modules_TRAFFIC is "true" #
+	// Return a reference to traffic object
+	public Traffic getTrafficRef()
+	{
+		return (this.traffic);
+	}
+	// #sijapp cond.end#
 
 
 }
