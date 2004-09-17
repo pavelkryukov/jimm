a1cb72dd42e366f88c7455fe816fc2c769ad886f
rossi771 <rossi771@9db0892b-294c-0410-b6c9-f1a9b70915b5>
2004-09-17 10:20:03 +0000

    Corrected header typo

--- src/jimm/comm/SearchAction.java
+++ src/jimm/comm/SearchAction.java
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ********************************************************************************
-File: src/jimm/Options.java
+File: src/jimm/comm/SearchAction.java
 Version: ###VERSION###  Date: ###DATE###
 Author(s): Andreas Rossbacher
 *******************************************************************************/
