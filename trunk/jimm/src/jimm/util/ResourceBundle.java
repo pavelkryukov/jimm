f78bc3739a99601cc0d6354a339dbd40e5c84029
rossi771 <rossi771@9db0892b-294c-0410-b6c9-f1a9b70915b5>
2005-03-09 21:41:12 +0000

    Made english the standard language again

--- trunk/jimm/src/jimm/util/ResourceBundle.java
+++ trunk/jimm/src/jimm/util/ResourceBundle.java
@@ -1,24 +1,24 @@
 /*******************************************************************************
- Jimm - J2ME ICQ client
- Copyright (C) 2003  Manuel Linsmayer
+Jimm - Mobile Messaging - J2ME ICQ clone
+Copyright (C) 2003-05  Jimm Project
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- ********************************************************************************
- File: src/jimm/util/ResourceBundle.java
- Version: ###VERSION###  Date: ###DATE###
- Author(s): Manuel Linsmayer, Andreas Rossbacher
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+********************************************************************************
+File: src/jimm/util/ResourceBundle.java
+Version: ###VERSION###  Date: ###DATE###
+Author(s): Manuel Linsmayer
 *******************************************************************************/
 
 
@@ -39,7 +39,10 @@ public class ResourceBundle
 	// List of available language packs
 	public static String[] LANG_AVAILABLE =
 	{
-		// #sijapp cond.if lang_BR is "true" #
+		// #sijapp cond.if lang_EN is "true" #
+		"EN",
+		// #sijapp cond.end #
+	    // #sijapp cond.if lang_BR is "true" #
 		"BR",
 		// #sijapp cond.end #
 		// #sijapp cond.if lang_BG is "true" #
@@ -50,9 +53,6 @@ public class ResourceBundle
 		// #sijapp cond.end #
 		// #sijapp cond.if lang_DE is "true" #
 		"DE",
-		// #sijapp cond.end #
-		// #sijapp cond.if lang_EN is "true" #
-		"EN",
 		// #sijapp cond.end #
 		// #sijapp cond.if lang_ES is "true" #
 		"ES",
