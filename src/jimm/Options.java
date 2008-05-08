c1725d73effa2dd73aca20985ca8c809d66ec46f
Rad1st <Rad1st@9db0892b-294c-0410-b6c9-f1a9b70915b5>
2008-05-08 09:07:33 +0000

    new color scheme "Green Night" updated

--- src/jimm/Options.java
+++ src/jimm/Options.java
@@ -209,7 +209,7 @@ public class Options
 		0xF9F3EF, 0xD60000, 0x3A6793, 0xE3BFA1, 0xC37D3F, 0xDB8941, 0xFFFFF0, 0xD26464, // Sand
 		0x000000, 0x00D000, 0xD0D0D0, 0x005000, 0x007000, 0x00FF00, 0x80FF80, 0x008000, // Hacker
 		0xD5FDFD, 0x000000, 0x009090, 0xB0FFFF, 0xA0FFFF, 0x0000FF, 0x000000, 0x00A0A0, // Aqua
-		0xFFFFFF, 0x0000AA, 0xFF7700, 0xDBE1E7, 0x648C64, 0x00D200, 0x000000, 0xB4B4B4, //
+		0x8CB29C, 0x002250, 0x101080, 0xDBE1E7, 0x648C64, 0x801010, 0x8CFB6B, 0xB4B4B4, // Green Night
 	};
 	
 
@@ -1059,7 +1059,7 @@ class OptionsForm implements CommandListener, ItemStateListener, VirtualListComm
 		JimmUI.addTextListItem(tlColorScheme, "Sand",           null, 5, true, -1, Font.STYLE_PLAIN);
 		JimmUI.addTextListItem(tlColorScheme, "Hacker :)",      null, 6, true, -1, Font.STYLE_PLAIN);
 		JimmUI.addTextListItem(tlColorScheme, "Aqua",           null, 7, true, -1, Font.STYLE_PLAIN);
-		JimmUI.addTextListItem(tlColorScheme, "Jungle",         null, 8, true, -1, Font.STYLE_PLAIN);
+		JimmUI.addTextListItem(tlColorScheme, "Green Night",    null, 8, true, -1, Font.STYLE_PLAIN);
 		
 		tlColorScheme.addCommandEx(JimmUI.cmdOk, VirtualList.MENU_TYPE_RIGHT_BAR);
 		tlColorScheme.addCommandEx(JimmUI.cmdCancel, VirtualList.MENU_TYPE_LEFT_BAR);
