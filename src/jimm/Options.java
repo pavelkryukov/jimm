8c6d463a20d1cce8e01591dfe940ec84201cff4f
arty333 <arty333@9db0892b-294c-0410-b6c9-f1a9b70915b5>
2008-05-09 16:35:58 +0000

    Another minor fix :)

--- src/jimm/Options.java
+++ src/jimm/Options.java
@@ -2136,7 +2136,7 @@ class OptionsForm implements CommandListener, ItemStateListener, VirtualListComm
 				switch (currOptMode)
 				{
 				case OPTIONS_RENAME_GROUP:
-					String groupName = Util.removeClRfAndTabs(groupSelector.getCurrText(0, false));
+					String groupName = Util.removeClRfAndTabs(groupSelector.getCurrText(0, false).trim());
 					showTextBoxForm("rename_group", "group_name", groupName, TextField.ANY);
 					optionsForm.addCommand(JimmUI.cmdOk);
 					optionsForm.addCommand(JimmUI.cmdBack);
