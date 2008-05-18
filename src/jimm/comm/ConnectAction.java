238e680703684b1dc826235dfcafd9d957f09244
arty333 <arty333@9db0892b-294c-0410-b6c9-f1a9b70915b5>
2008-05-18 18:07:55 +0000

    Fixed a typo with phantoms types

--- src/jimm/comm/ConnectAction.java
+++ src/jimm/comm/ConnectAction.java
@@ -664,9 +664,8 @@ public class ConnectAction extends Action
 							if (
 								(type == 0x0000)
 								 || (Options.getBoolean(Options.OPTION_SHOW_DELETED_CONT) &&
-								((type == 0x0019) || (type == 0x001C)))
+								((type == 0x0019) || (type == 0x001B)))
 							)
-
 							{
 								ByteArrayOutputStream serverData = new ByteArrayOutputStream();
 								
