760960c74c9dd2949d3897cb53d83f816f114112
Rad1st <Rad1st@9db0892b-294c-0410-b6c9-f1a9b70915b5>
2008-10-18 12:46:30 +0000

    reformatted PhoneBook.java

--- trunk/jimm/src/jimm/PhoneBook.java
+++ trunk/jimm/src/jimm/PhoneBook.java
@@ -44,266 +44,265 @@ import jimm.util.ResourceBundle;
 
 public class PhoneBook implements CommandListener {
 
-    private static PhoneBook _this;
-    private static TextList list;
-    private static TextBox smsText;
-    private static TextBox numText;
-    private static MessageConnection connection;
-    private static TelNum[] allTelNum;
-    private static Vector data = new Vector();
-    private static final Command backCommand = new Command(ResourceBundle.getString("back"), Jimm.cmdBack, 1);
-    //#sijapp cond.if target!="DEFAULT"#
-    private static final Command cmdCall = new Command(ResourceBundle.getString("call"), Command.ITEM, 4);
-    //#sijapp cond.end#
-    private static final Command cmdWriteSMS = new Command(ResourceBundle.getString("write_sms"), Command.ITEM, 5);
-    private static final Command cmdSendSMS = new Command(ResourceBundle.getString("send_sms"), Command.ITEM, 1);
-    private static final Command cmdEditNum = new Command(ResourceBundle.getString("edit_num"), Command.ITEM, 6);
-    static int counter = 0;
-
-    public PhoneBook() {
-	_this = this;
-    }
-
-    public static void addContactToList(String name, String number, int value) {
-	synchronized (list) {
-	    list.addBigText(name + ":", list.getTextColor(), Font.STYLE_BOLD, value);
-	    list.doCRLF(value);
-	    list.addBigText(number, Options.getSchemeColor(Options.CLRSCHHEME_OUTGOING, -1), Font.STYLE_PLAIN, value);
-	    list.doCRLF(value);
-	}
-    }
-
-    private static void fillPhonesInList() {
-	list.lock();
-	list.clear();
-	for (int i = 0; i < allTelNum.length; i++) {
-	    addContactToList(allTelNum[i].getName(), allTelNum[i].getTel(), allTelNum[i].getValue());
-	}
-	list.unlock();
-    }
-
-    private static void addTelNumInfo(Vector vct, String name, String tel, int value) {
-	vct.addElement(new TelNum(name, tel, value));
-    }
-
-    public static void activate(boolean restorePos) {
-	String cap = null;
-	int lastPos = -1;
-
-	if (list == null) {
-	    list = new TextList(ResourceBundle.getString(cap));
-	}
-
-	if (restorePos) {
-	    lastPos = list.getCurrTextIndex();
-	}
-
-	list.setMode(VirtualList.CURSOR_MODE_DISABLED);
-	list.setCaption(ResourceBundle.getString("phone_book"));
-	JimmUI.setColorScheme(list, false, -1, true);
-	list.setCyclingCursor(true);
-	list.addCommandEx(backCommand, VirtualList.MENU_TYPE_LEFT_BAR);
-	list.addCommandEx(JimmUI.cmdMenu, VirtualList.MENU_TYPE_RIGHT_BAR);
+	private static PhoneBook _this;
+	private static TextList list;
+	private static TextBox smsText;
+	private static TextBox numText;
+	private static MessageConnection connection;
+	private static TelNum[] allTelNum;
+	private static Vector data = new Vector();
+	private static final Command backCommand = new Command(ResourceBundle.getString("back"), Jimm.cmdBack, 1);
 	//#sijapp cond.if target!="DEFAULT"#
-	list.addCommandEx(cmdCall, VirtualList.MENU_TYPE_RIGHT);
+	private static final Command cmdCall = new Command(ResourceBundle.getString("call"), Command.ITEM, 4);
 	//#sijapp cond.end#
-	list.addCommandEx(cmdWriteSMS, VirtualList.MENU_TYPE_RIGHT);
-	list.addCommandEx(cmdEditNum, VirtualList.MENU_TYPE_RIGHT);
-	list.setCommandListener(_this);
+	private static final Command cmdWriteSMS = new Command(ResourceBundle.getString("write_sms"), Command.ITEM, 5);
+	private static final Command cmdSendSMS = new Command(ResourceBundle.getString("send_sms"), Command.ITEM, 1);
+	private static final Command cmdEditNum = new Command(ResourceBundle.getString("edit_num"), Command.ITEM, 6);
+	static int counter = 0;
 
-	if (counter == 0) {
-	    addTelNumInfo(data, ResourceBundle.getString("enter_phone_num"), "", counter);
-	    counter++;
-	    // load the list of names in a different thread
-	    new Thread(new LoadContacts()).start();
-	} else {
-	    fillPhonesInList();
+	public PhoneBook() {
+		_this = this;
 	}
-	if (restorePos) {
-	    list.selectTextByIndex(lastPos);
+
+	public static void addContactToList(String name, String number, int value) {
+		synchronized (list) {
+			list.addBigText(name + ":", list.getTextColor(), Font.STYLE_BOLD, value);
+			list.doCRLF(value);
+			list.addBigText(number, Options.getSchemeColor(Options.CLRSCHHEME_OUTGOING, -1), Font.STYLE_PLAIN, value);
+			list.doCRLF(value);
+		}
 	}
-	list.activate(Jimm.display);
-    }
 
-    private String getCurrPhoneNumber() {
-	return allTelNum[list.getCurrTextIndex()].getTel();
-    }
+	private static void fillPhonesInList() {
+		list.lock();
+		list.clear();
+		for (int i = 0; i < allTelNum.length; i++) {
+			addContactToList(allTelNum[i].getName(), allTelNum[i].getTel(), allTelNum[i].getValue());
+		}
+		list.unlock();
+	}
 
-    private void setCurrPhoneNumber(String number) {
-	allTelNum[list.getCurrTextIndex()].setTel(number);
-    }
+	private static void addTelNumInfo(Vector vct, String name, String tel, int value) {
+		vct.addElement(new TelNum(name, tel, value));
+	}
 
-    /**
-     * Sets the destination address and payload text for the text SMS.
-     */
-    private TextMessage prepareSMS(String number) {
-	TextMessage message = (TextMessage) connection.newMessage(
-		MessageConnection.TEXT_MESSAGE);
-	message.setAddress("sms://" + number);
-	String text = smsText.getString();
-	message.setPayloadText(text);
+	public static void activate(boolean restorePos) {
+		String cap = null;
+		int lastPos = -1;
 
-	return message;
-    }
+		if (list == null) {
+			list = new TextList(ResourceBundle.getString(cap));
+		}
 
-    /**
-     * Sends a text SMS.
-     */
-    private void sendSMS(final TextMessage message) {
-	Thread smsThread = new Thread() {
+		if (restorePos) {
+			lastPos = list.getCurrTextIndex();
+		}
 
-	    public void run() {
-		try {
-		    connection.send(message);
-		} catch (InterruptedIOException ex) {
-		    // TODO: Exception (e.g. timeout) handling
-		    } catch (IOException ex) {
-		    // TODO: Exception (e.g. network failure) handling
-		    } catch (IllegalArgumentException ex) {
-		    // TODO: Exception (e.g. too big or otherwise invalid
-		    // message) handling
-		    } catch (SecurityException ex) {
-		    // TODO: Exception (e.g. insufficient permissions) handling
-		    }
-	    }
-	};
-	smsThread.start();
-    }
+		list.setMode(VirtualList.CURSOR_MODE_DISABLED);
+		list.setCaption(ResourceBundle.getString("phone_book"));
+		JimmUI.setColorScheme(list, false, -1, true);
+		list.setCyclingCursor(true);
+		list.addCommandEx(backCommand, VirtualList.MENU_TYPE_LEFT_BAR);
+		list.addCommandEx(JimmUI.cmdMenu, VirtualList.MENU_TYPE_RIGHT_BAR);
+		//#sijapp cond.if target!="DEFAULT"#
+		list.addCommandEx(cmdCall, VirtualList.MENU_TYPE_RIGHT);
+		//#sijapp cond.end#
+		list.addCommandEx(cmdWriteSMS, VirtualList.MENU_TYPE_RIGHT);
+		list.addCommandEx(cmdEditNum, VirtualList.MENU_TYPE_RIGHT);
+		list.setCommandListener(_this);
 
-    public void commandAction(Command c, Displayable d) {
-	Jimm.aaUserActivity();
+		if (counter == 0) {
+			addTelNumInfo(data, ResourceBundle.getString("enter_phone_num"), "", counter);
+			counter++;
+			// load the list of names in a different thread
+			new Thread(new LoadContacts()).start();
+		} else {
+			fillPhonesInList();
+		}
+		if (restorePos) {
+			list.selectTextByIndex(lastPos);
+		}
+		list.activate(Jimm.display);
+	}
 
-	if (c == backCommand) {
-	    RunnableImpl.backToLastScreenMT();
+	private String getCurrPhoneNumber() {
+		return allTelNum[list.getCurrTextIndex()].getTel();
+	}
+
+	private void setCurrPhoneNumber(String number) {
+		allTelNum[list.getCurrTextIndex()].setTel(number);
+	}
+
+	/**
+	 * Sets the destination address and payload text for the text SMS.
+	 */
+	private TextMessage prepareSMS(String number) {
+		TextMessage message = (TextMessage) connection.newMessage(
+				MessageConnection.TEXT_MESSAGE);
+		message.setAddress("sms://" + number);
+		String text = smsText.getString();
+		message.setPayloadText(text);
+
+		return message;
+	}
+
+	/**
+	 * Sends a text SMS.
+	 */
+	private void sendSMS(final TextMessage message) {
+		Thread smsThread = new Thread() {
+
+			public void run() {
+				try {
+					connection.send(message);
+				} catch (InterruptedIOException ex) {
+					// TODO: Exception (e.g. timeout) handling
+				} catch (IOException ex) {
+					// TODO: Exception (e.g. network failure) handling
+				} catch (IllegalArgumentException ex) {
+					// TODO: Exception (e.g. too big or otherwise invalid
+					// message) handling
+				} catch (SecurityException ex) {
+					// TODO: Exception (e.g. insufficient permissions) handling
+				}
+			}
+		};
+		smsThread.start();
+	}
+
+	public void commandAction(Command c, Displayable d) {
+		Jimm.aaUserActivity();
+
+		if (c == backCommand) {
+			RunnableImpl.backToLastScreenMT();
+
+		//#sijapp cond.if target!="DEFAULT"#
+		} else if (c == cmdCall) {
+			String number = getCurrPhoneNumber();
+			if (number.length() > 0) {
+				try {
+					Jimm.jimm.platformRequest("tel:" + number);
+				} catch (Exception ignore) {/*Do nothing*/
+
+				}
+			}
+		//#sijapp cond.end#
+
+		} else if (c == cmdWriteSMS) {
+			String number = getCurrPhoneNumber();
+			smsText = new TextBox(null, "", 500, TextField.ANY);
+			smsText.addCommand(cmdSendSMS);
+			smsText.addCommand(backCommand);
+			smsText.setCommandListener(this);
+			Jimm.display.setCurrent(smsText);
+
+		} else if (c == cmdSendSMS) {
+			String number = getCurrPhoneNumber();
+			if (number.length() > 0) {
+				RunnableImpl.backToLastScreenMT();
+
+				try {
+					connection = (MessageConnection) Connector.open("sms://:5000", Connector.WRITE);
+				} catch (IOException ex) {
+					// TODO: Exception handling
+				}
+
+				TextMessage message = prepareSMS(number);
+				sendSMS(message);
+			}
+
+		} else if (c == cmdEditNum && list.getCurrTextIndex() == 0) {
+			String number = getCurrPhoneNumber();
+			numText = new TextBox(null, number, 20, TextField.PHONENUMBER);
+			numText.addCommand(JimmUI.cmdOk);
+			numText.addCommand(JimmUI.cmdCancel);
+			numText.setCommandListener(this);
+			Jimm.display.setCurrent(numText);
+
+		} else if (numText != null && numText.isShown()) {
+			if (c == JimmUI.cmdOk) {
+				setCurrPhoneNumber(numText.getString());
+			}
+			PhoneBook.activate(true);
+			numText = null;
+		}
+	}
 
 	//#sijapp cond.if target!="DEFAULT"#
-	} else if (c == cmdCall) {
-	    String number = getCurrPhoneNumber();
-	    if (number.length() > 0) {
+	private static void loadNames(String name)
+			throws PIMException, SecurityException {
+		javax.microedition.pim.ContactList cl = null;
 		try {
-		    Jimm.jimm.platformRequest("tel:" + number);
-		} catch (Exception ignore) {/*Do nothing*/
+			cl = (javax.microedition.pim.ContactList) PIM.getInstance().openPIMList(PIM.CONTACT_LIST, PIM.READ_ONLY, name);
 
-		}
-	    }
-        //#sijapp cond.end#
+			if (cl.isSupportedField(Contact.FORMATTED_NAME) && cl.isSupportedField(Contact.TEL)) {
+				// TODO: Progress bar here
+				Enumeration items = cl.items();
+				Vector telNumbers = new Vector();
+				while (items.hasMoreElements()) {
+					Contact contact = (Contact) items.nextElement();
+					int telCount = contact.countValues(Contact.TEL);
+					int nameCount = contact.countValues(Contact.FORMATTED_NAME);
 
-	} else if (c == cmdWriteSMS) {
-	    String number = getCurrPhoneNumber();
-	    smsText = new TextBox(null, "", 500, TextField.ANY);
-	    smsText.addCommand(cmdSendSMS);
-	    smsText.addCommand(backCommand);
-	    smsText.setCommandListener(this);
-	    Jimm.display.setCurrent(smsText);
-
-	} else if (c == cmdSendSMS) {
-	    String number = getCurrPhoneNumber();
-	    if (number.length() > 0) {
-		RunnableImpl.backToLastScreenMT();
-
-		try {
-		    connection = (MessageConnection) Connector.open("sms://:5000", Connector.WRITE);
-		} catch (IOException ex) {
-		    // TODO: Exception handling
-		}
-
-		TextMessage message = prepareSMS(number);
-		sendSMS(message);
-	    }
-
-	} else if (c == cmdEditNum && list.getCurrTextIndex() == 0) {
-	    String number = getCurrPhoneNumber();
-	    numText = new TextBox(null, number, 20, TextField.PHONENUMBER);
-	    numText.addCommand(JimmUI.cmdOk);
-	    numText.addCommand(JimmUI.cmdCancel);
-	    numText.setCommandListener(this);
-	    Jimm.display.setCurrent(numText);
-
-	} else if (numText != null && numText.isShown()) {
-	    if (c == JimmUI.cmdOk) {
-		setCurrPhoneNumber(numText.getString());
-	    }
-	    PhoneBook.activate(true);
-	    numText = null;
-	}
-    }
-
-    //#sijapp cond.if target!="DEFAULT"#
-    private static void loadNames(String name)
-	    throws PIMException, SecurityException {
-	javax.microedition.pim.ContactList cl = null;
-	try {
-	    cl = (javax.microedition.pim.ContactList) PIM.getInstance().openPIMList(PIM.CONTACT_LIST, PIM.READ_ONLY, name);
-
-	    if (cl.isSupportedField(Contact.FORMATTED_NAME) && cl.isSupportedField(Contact.TEL)) {
-		// TODO: Progress bar here
-		Enumeration items = cl.items();
-		Vector telNumbers = new Vector();
-		while (items.hasMoreElements()) {
-		    Contact contact = (Contact) items.nextElement();
-		    int telCount = contact.countValues(Contact.TEL);
-		    int nameCount = contact.countValues(Contact.FORMATTED_NAME);
-
-		    if (telCount > 0 && nameCount > 0) {
-			String contactName =
-				contact.getString(Contact.FORMATTED_NAME, 0);
-			for (int i = 0; i < telCount; i++) {
-			    int telAttributes =
-				    contact.getAttributes(Contact.TEL, i);
-			    String telNumber =
-				    contact.getString(Contact.TEL, i);
-			    if (cl.isSupportedAttribute(Contact.TEL,
-				    Contact.ATTR_MOBILE)) {
-				if ((telAttributes & Contact.ATTR_MOBILE) != 0) {
-				    telNumbers.insertElementAt(telNumber, 0);
-				} else {
-				    telNumbers.addElement(telNumber);
+					if (telCount > 0 && nameCount > 0) {
+						String contactName =
+								contact.getString(Contact.FORMATTED_NAME, 0);
+						for (int i = 0; i < telCount; i++) {
+							int telAttributes =
+									contact.getAttributes(Contact.TEL, i);
+							String telNumber =
+									contact.getString(Contact.TEL, i);
+							if (cl.isSupportedAttribute(Contact.TEL,
+									Contact.ATTR_MOBILE)) {
+								if ((telAttributes & Contact.ATTR_MOBILE) != 0) {
+									telNumbers.insertElementAt(telNumber, 0);
+								} else {
+									telNumbers.addElement(telNumber);
+								}
+							} else {
+								telNumbers.addElement(telNumber);
+							}
+						}
+						if (contactName.length() > 20) {
+							contactName = contactName.substring(0, 17) + "...";
+						}
+						for (int i = 0; i < telNumbers.size(); i++) {
+							addTelNumInfo(data, contactName, (String) telNumbers.elementAt(i), counter);
+							counter++;
+						}
+						telNumbers.removeAllElements();
+					}
 				}
-			    } else {
-				telNumbers.addElement(telNumber);
-			    }
 			}
-			if (contactName.length() > 20) {
-			    contactName = contactName.substring(0, 17) + "...";
+		} finally {
+			if (cl != null) {
+				cl.close();
 			}
-			for (int i = 0; i < telNumbers.size(); i++) {
-			    addTelNumInfo(data, contactName, (String) telNumbers.elementAt(i), counter);
-			    counter++;
-			}
-			telNumbers.removeAllElements();
-		    }
 		}
-	    }
-	} finally {
-	    if (cl != null) {
-		cl.close();
-	    }
 	}
-    }
-    //#sijapp cond.end#
+	//#sijapp cond.end#
+	private static class LoadContacts implements Runnable {
 
-    private static class LoadContacts implements Runnable {
-
-	public void run() {
-	    //#sijapp cond.if target!="DEFAULT"#
-	    try {
-		String[] allContactLists = PIM.getInstance().listPIMLists(PIM.CONTACT_LIST);
-		if (allContactLists.length != 0) {
-		    for (int i = 0; i < allContactLists.length; i++) {
-			loadNames(allContactLists[i]);
-		    }
+		public void run() {
+			//#sijapp cond.if target!="DEFAULT"#
+			try {
+				String[] allContactLists = PIM.getInstance().listPIMLists(PIM.CONTACT_LIST);
+				if (allContactLists.length != 0) {
+					for (int i = 0; i < allContactLists.length; i++) {
+						loadNames(allContactLists[i]);
+					}
+				}
+			} catch (PIMException e) {
+				DebugLog.addText("PhoneBook: " + e.getMessage());
+			} catch (SecurityException e) {
+				DebugLog.addText("PhoneBook: " + e.getMessage());
+			}
+			//#sijapp cond.end#
+			allTelNum = new TelNum[data.size()];
+			data.copyInto(allTelNum);
+			data.removeAllElements();
+			fillPhonesInList();
 		}
-	    } catch (PIMException e) {
-		DebugLog.addText("PhoneBook: " + e.getMessage());
-	    } catch (SecurityException e) {
-		DebugLog.addText("PhoneBook: " + e.getMessage());
-	    }
-	    //#sijapp cond.end#
-	    allTelNum = new TelNum[data.size()];
-	    data.copyInto(allTelNum);
-	    data.removeAllElements();
-	    fillPhonesInList();
 	}
-    }
 }
 //#sijapp cond.end#
