58d3d8c4812d98481289b8459d62f70a3ab5b660
Rad1st <Rad1st@9db0892b-294c-0410-b6c9-f1a9b70915b5>
2008-10-25 18:28:25 +0000

    bugfix: jimm crashed at loading on device without support of PIM and SMS api (PIM module)

--- trunk/jimm/src/jimm/PhoneBook.java
+++ trunk/jimm/src/jimm/PhoneBook.java
@@ -60,6 +60,24 @@ public class PhoneBook implements CommandListener {
 	private static final Command cmdEditNum = new Command(ResourceBundle.getString("edit_num"), Command.ITEM, 6);
 	static int counter = 0;
 
+	private static boolean PIM_API_Available;
+	private static boolean SMS_API_Available;
+
+	static {
+		String pimVersion = System.getProperty("microedition.pim.version");
+		if (pimVersion != null) {
+			PIM_API_Available = true;
+		} else {
+			PIM_API_Available = false;
+		}
+		String wmVersion = System.getProperty("wireless.messaging.version");
+		if (wmVersion != null) {
+			SMS_API_Available = true;
+		} else {
+			SMS_API_Available = false;
+		}
+	}
+
 	public PhoneBook() {
 		_this = this;
 	}
@@ -107,20 +125,25 @@ public class PhoneBook implements CommandListener {
 		list.setCaption(ResourceBundle.getString("phone_book"));
 		JimmUI.setColorScheme(list, false, -1, true);
 		list.setCyclingCursor(true);
-		list.addCommandEx(backCommand, VirtualList.MENU_TYPE_LEFT_BAR);
-		list.addCommandEx(JimmUI.cmdMenu, VirtualList.MENU_TYPE_RIGHT_BAR);
-		//#sijapp cond.if target!="DEFAULT"#
-		list.addCommandEx(cmdCall, VirtualList.MENU_TYPE_RIGHT);
-		//#sijapp cond.end#
-		list.addCommandEx(cmdWriteSMS, VirtualList.MENU_TYPE_RIGHT);
-		list.addCommandEx(cmdEditNum, VirtualList.MENU_TYPE_RIGHT);
+		list.addCommandEx(cmdCall, VirtualList.MENU_TYPE_RIGHT_BAR);
+		list.addCommandEx(JimmUI.cmdMenu, VirtualList.MENU_TYPE_LEFT_BAR);
+		if (SMS_API_Available) {
+			list.addCommandEx(cmdWriteSMS, VirtualList.MENU_TYPE_LEFT);
+		}
+		list.addCommandEx(cmdEditNum, VirtualList.MENU_TYPE_LEFT);
+		list.addCommandEx(backCommand, VirtualList.MENU_TYPE_LEFT);
 		list.setCommandListener(_this);
 
 		if (counter == 0) {
 			addTelNumInfo(data, ResourceBundle.getString("enter_phone_num"), "", counter);
 			counter++;
-			// load the list of names in a different thread
-			new Thread(new LoadContacts()).start();
+			if (PIM_API_Available)
+			{
+				// load the list of names in a different thread
+				new Thread(new LoadContacts()).start();
+			} else {
+				moveDataToList();
+			}
 		} else {
 			fillPhonesInList();
 		}
@@ -130,6 +153,13 @@ public class PhoneBook implements CommandListener {
 		list.activate(Jimm.display);
 	}
 
+	private static void moveDataToList()
+	{
+		allTelNum = new TelNum[data.size()];
+		data.copyInto(allTelNum);
+		data.removeAllElements();
+		fillPhonesInList();
+	}
 	private String getCurrPhoneNumber() {
 		int idx = list.getCurrTextIndex();
 		if ((idx > -1) && (idx < allTelNum.length))
@@ -140,43 +170,6 @@ public class PhoneBook implements CommandListener {
 
 	private void setCurrPhoneNumber(String number) {
 		allTelNum[list.getCurrTextIndex()].setTel(number);
-	}
-
-	/**
-	 * Sets the destination address and payload text for the text SMS.
-	 */
-	private TextMessage prepareSMS(String number) {
-		TextMessage message = (TextMessage) connection.newMessage(
-				MessageConnection.TEXT_MESSAGE);
-		message.setAddress("sms://" + number);
-		String text = smsText.getString();
-		message.setPayloadText(text);
-
-		return message;
-	}
-
-	/**
-	 * Sends a text SMS.
-	 */
-	private void sendSMS(final TextMessage message) {
-		Thread smsThread = new Thread() {
-
-			public void run() {
-				try {
-					connection.send(message);
-				} catch (InterruptedIOException ex) {
-					// TODO: Exception (e.g. timeout) handling
-				} catch (IOException ex) {
-					// TODO: Exception (e.g. network failure) handling
-				} catch (IllegalArgumentException ex) {
-					// TODO: Exception (e.g. too big or otherwise invalid
-					// message) handling
-				} catch (SecurityException ex) {
-					// TODO: Exception (e.g. insufficient permissions) handling
-				}
-			}
-		};
-		smsThread.start();
 	}
 
 	public void commandAction(Command c, Displayable d) {
@@ -191,9 +184,7 @@ public class PhoneBook implements CommandListener {
 			if (number.length() > 0) {
 				try {
 					Jimm.jimm.platformRequest("tel:" + number);
-				} catch (Exception ignore) {/*Do nothing*/
-
-				}
+				} catch (Exception ignore) {/*Do nothing*/}
 			}
 		//#sijapp cond.end#
 
@@ -209,15 +200,7 @@ public class PhoneBook implements CommandListener {
 			String number = getCurrPhoneNumber();
 			if (number.length() > 0) {
 				RunnableImpl.backToLastScreenMT();
-
-				try {
-					connection = (MessageConnection) Connector.open("sms://:5000", Connector.WRITE);
-				} catch (IOException ex) {
-					// TODO: Exception handling
-				}
-
-				TextMessage message = prepareSMS(number);
-				sendSMS(message);
+				new Thread(new sms (smsText.getString(), number)).start();
 			}
 
 		} else if (c == cmdEditNum && list.getCurrTextIndex() == 0) {
@@ -237,112 +220,97 @@ public class PhoneBook implements CommandListener {
 		}
 	}
 
+	private static class LoadContacts implements Runnable {
+
 //#sijapp cond.if target!="DEFAULT"#
-	
-	private static String readPimName(Contact contact, int nameConstant)
-	{
-		switch (nameConstant)
-		{
-		case Contact.FORMATTED_NAME:
-			return contact.getString(nameConstant, 0);
+		private static String readPimName(Contact contact, int nameConstant) {
+			switch (nameConstant) {
+				case Contact.FORMATTED_NAME:
+					return contact.getString(nameConstant, 0);
 
-		case Contact.NAME:
-			int nameNumbers = contact.countValues(Contact.NAME);
-			StringBuffer sb = new StringBuffer();
+				case Contact.NAME:
+					int nameNumbers = contact.countValues(Contact.NAME);
+					StringBuffer sb = new StringBuffer();
 
-			for (int i = 0; i < nameNumbers; i++)
-			{
+					for (int i = 0; i < nameNumbers; i++) {
 
-				String[] nameData = contact.getStringArray(Contact.NAME, i);
+						String[] nameData = contact.getStringArray(Contact.NAME, i);
 
-				if (nameData != null)
-				{
+						if (nameData != null) {
 
-					if (nameData[Contact.NAME_GIVEN] != null) sb.append(nameData[Contact.NAME_GIVEN]);
-
-					if (nameData[Contact.NAME_FAMILY] != null)
-					{
-						if (sb.length() > 0)
-							sb.append(" ");
-						sb.append(nameData[Contact.NAME_FAMILY]);
+							if (nameData[Contact.NAME_GIVEN] != null) {
+								sb.append(nameData[Contact.NAME_GIVEN]);
+							}
+							if (nameData[Contact.NAME_FAMILY] != null) {
+								if (sb.length() > 0) {
+									sb.append(" ");
+								}
+								sb.append(nameData[Contact.NAME_FAMILY]);
+							}
+						}
 					}
-				}
+					return sb.toString().trim();
 			}
-			return sb.toString().trim();
+			return null;
 		}
-		return null;
-	}
-	
-	private static void loadNames(String name) throws PIMException, SecurityException
-	{
-		javax.microedition.pim.ContactList cl = null;
-		try
-		{
-			if (name != null)
-				cl = (javax.microedition.pim.ContactList) PIM.getInstance().openPIMList(PIM.CONTACT_LIST, PIM.READ_ONLY, name);
-			else
-				cl = (javax.microedition.pim.ContactList) PIM.getInstance().openPIMList(PIM.CONTACT_LIST, PIM.READ_ONLY);
-			
-			int nameConstant;
-			
-			if (cl.isSupportedField(Contact.FORMATTED_NAME)) nameConstant = Contact.FORMATTED_NAME;
-			else if (cl.isSupportedField(Contact.NAME)) nameConstant = Contact.NAME;
-			else return;
 
-			// TODO: Progress bar here
-			Enumeration items = cl.items();
-			Vector telNumbers = new Vector();
-			while (items.hasMoreElements())
-			{
-				Contact contact = (Contact) items.nextElement();
-				int telCount = contact.countValues(Contact.TEL);
-				
-				int nameCount = contact.countValues(nameConstant);
+		private static void loadNames(String name) throws PIMException, SecurityException {
+			javax.microedition.pim.ContactList cl = null;
+			try {
+				if (name != null) {
+					cl = (javax.microedition.pim.ContactList) PIM.getInstance().openPIMList(PIM.CONTACT_LIST, PIM.READ_ONLY, name);
+				} else {
+					cl = (javax.microedition.pim.ContactList) PIM.getInstance().openPIMList(PIM.CONTACT_LIST, PIM.READ_ONLY);
+				}
+				int nameConstant;
 
-				if (telCount > 0 && nameCount > 0)
-				{
-					String contactName = readPimName(contact, nameConstant);
-					for (int i = 0; i < telCount; i++)
-					{
-						int telAttributes = contact.getAttributes(Contact.TEL, i);
-						String telNumber = contact.getString(Contact.TEL, i);
-						if (cl.isSupportedAttribute(Contact.TEL, Contact.ATTR_MOBILE))
-						{
-							if ((telAttributes & Contact.ATTR_MOBILE) != 0)
-							{
-								telNumbers.insertElementAt(telNumber, 0);
-							} else
-							{
+				if (cl.isSupportedField(Contact.FORMATTED_NAME)) {
+					nameConstant = Contact.FORMATTED_NAME;
+				} else if (cl.isSupportedField(Contact.NAME)) {
+					nameConstant = Contact.NAME;
+				} else {
+					return;				// TODO: Progress bar here
+				}
+				Enumeration items = cl.items();
+				Vector telNumbers = new Vector();
+				while (items.hasMoreElements()) {
+					Contact contact = (Contact) items.nextElement();
+					int telCount = contact.countValues(Contact.TEL);
+
+					int nameCount = contact.countValues(nameConstant);
+
+					if (telCount > 0 && nameCount > 0) {
+						String contactName = readPimName(contact, nameConstant);
+						for (int i = 0; i < telCount; i++) {
+							int telAttributes = contact.getAttributes(Contact.TEL, i);
+							String telNumber = contact.getString(Contact.TEL, i);
+							if (cl.isSupportedAttribute(Contact.TEL, Contact.ATTR_MOBILE)) {
+								if ((telAttributes & Contact.ATTR_MOBILE) != 0) {
+									telNumbers.insertElementAt(telNumber, 0);
+								} else {
+									telNumbers.addElement(telNumber);
+								}
+							} else {
 								telNumbers.addElement(telNumber);
 							}
 						}
-						else telNumbers.addElement(telNumber);
+						if (contactName.length() > 20) {
+							contactName = contactName.substring(0, 17) + "...";
+						}
+						for (int i = 0; i < telNumbers.size(); i++) {
+							addTelNumInfo(data, contactName, (String) telNumbers.elementAt(i), counter);
+							counter++;
+						}
+						telNumbers.removeAllElements();
 					}
-					if (contactName.length() > 20)
-					{
-						contactName = contactName.substring(0, 17) + "...";
-					}
-					for (int i = 0; i < telNumbers.size(); i++)
-					{
-						addTelNumInfo(data, contactName, (String) telNumbers.elementAt(i), counter);
-						counter++;
-					}
-					telNumbers.removeAllElements();
+				}
+			} finally {
+				if (cl != null) {
+					cl.close();
 				}
 			}
-		} finally
-		{
-			if (cl != null)
-			{
-				cl.close();
-			}
 		}
-	}
-
-	//#sijapp cond.end#
-	
-	private static class LoadContacts implements Runnable {
-
+		//#sijapp cond.end#
 		public void run() {
 			//#sijapp cond.if target!="DEFAULT"#
 			try {
@@ -351,18 +319,54 @@ public class PhoneBook implements CommandListener {
 					for (int i = 0; i < allContactLists.length; i++) {
 						loadNames(allContactLists[i]);
 					}
+				} else {
+					loadNames(null);
 				}
-				else loadNames(null);
 			} catch (PIMException e) {
 				DebugLog.addText("PhoneBook: " + e.getMessage());
 			} catch (SecurityException e) {
 				DebugLog.addText("PhoneBook: " + e.getMessage());
 			}
-			//#sijapp cond.end#
-			allTelNum = new TelNum[data.size()];
-			data.copyInto(allTelNum);
-			data.removeAllElements();
-			fillPhonesInList();
+			moveDataToList();
+		//#sijapp cond.end#
+		}
+	}
+
+	private class sms implements Runnable {
+
+		private TextMessage message;
+		
+		public sms(String message_, String number_) {
+			
+			try {
+				connection = (MessageConnection) Connector.open("sms://:5000", Connector.WRITE);
+			} catch (IOException ex) {
+				// TODO: Exception handling
+			}
+
+			message = (TextMessage) connection.newMessage(
+					MessageConnection.TEXT_MESSAGE);
+			message.setAddress("sms://" + number_);
+			String text = message_;
+			message.setPayloadText(text);
+		}
+
+		/**
+		 * Sends a text SMS.
+		 */
+		public void run() {
+			try {
+				connection.send(message);
+			} catch (InterruptedIOException ex) {
+				// TODO: Exception (e.g. timeout) handling
+			} catch (IOException ex) {
+				// TODO: Exception (e.g. network failure) handling
+			} catch (IllegalArgumentException ex) {
+				// TODO: Exception (e.g. too big or otherwise invalid
+				// message) handling
+			} catch (SecurityException ex) {
+				// TODO: Exception (e.g. insufficient permissions) handling
+			}
 		}
 	}
 }
